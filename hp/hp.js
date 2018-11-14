9f374f08f99ba5e0c38cf7ef3e5f5ca03114d395
Lucy <lewright20@gmail.com>
2018-11-14 15:29:18 +0000

    first api call set up

--- hp/hp.js
+++ hp/hp.js
@@ -1,1 +1,21 @@
+() => {
+  const makeRequest = (url, callback) => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        let obj = JSON.parse(xhr.responseText);
+        callback(obj);
+      }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+  };
 
+  const hpNameUrl = 'hp-api.herokuapp.com/api/characters';
+
+  const hpNameCallback = obj => {
+    document.getElementById('hp-user-handle').textContent = obj[0].name;
+  };
+
+  makeRequest(hpNameUrl, hpNameCallback);
+};
