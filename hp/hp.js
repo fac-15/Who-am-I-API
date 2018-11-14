5cfd9739db441c72710920626639da0fcad1d06e
Lucy <lewright20@gmail.com>
2018-11-14 17:59:52 +0000

    basic html in place, most hp api working

--- hp/hp.js
+++ hp/hp.js
@@ -1,4 +1,4 @@
-() => {
+(() => {
   const makeRequest = (url, callback) => {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
@@ -10,12 +10,26 @@
     xhr.open('GET', url, true);
     xhr.send();
   };
+  const hpObjUrl = 'http://hp-api.herokuapp.com/api/characters';
 
-  const hpNameUrl = 'hp-api.herokuapp.com/api/characters';
-
-  const hpNameCallback = obj => {
+  const hpNameCallback = (obj) => {
     document.getElementById('hp-user-handle').textContent = obj[0].name;
+    document.getElementById('hp-character-species').textContent =
+      obj[0].species;
+    document.getElementById('hp-character-house').textContent = obj[0].house;
+    document.getElementById('hp-character-history').textContent =
+      obj[0].ancestry;
+    document.getElementById('hp-character-wand').textContent = Object.values(
+      obj[0].wand
+    )
+      .slice(0, 2)
+      .join(' and ');
+    document.getElementById(
+      'hp-character-wand-length'
+    ).textContent = Object.values(obj[0].wand).pop();
+    document.getElementById('hp-character-patronus').textContent =
+      obj[0].patronus;
   };
 
-  makeRequest(hpNameUrl, hpNameCallback);
-};
+  makeRequest(hpObjUrl, hpNameCallback);
+})();
