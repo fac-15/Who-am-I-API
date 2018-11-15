ae0941787955049b0678794d7a4fbe61978d9277
Sandra <grahlsan@gmail.com>
2018-11-15 10:03:31 +0000

    testing

--- github/github.js
+++ github/github.js
@@ -1,1 +1,26 @@
 
+// let url = "https://api.github.com/users/"; // + input.value
+
+
+function getUserName() {
+
+};
+
+
+// let fetchingFunction = (function() {
+
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.onreadystatechange = function() {
+//         if (xhr.readyState == 4 && xhr.status == 200) {
+//             let gitHubphoto = JSON.parse(xhr.responseText);
+//             let photo = document.getElementById("#");
+//             let link = githubPhoto.avatar_url;
+//             photo.src = link;
+//         }
+//     };
+//     xhr.open("GET", url, true);
+//     xhr.send();
+//     })();
+
+module.exports = function getUserName() {};
--- github/github.test.js
+++ github/github.test.js
@@ -1,1 +1,24 @@
 
+const test = require("tape");
+const gitHub = require("./github.js");
+
+
+// test("Example test", function(t) {
+//     t.pass();
+//     t.end();
+// });
+
+// test('fetchingFunction() should return an object', function(t) {
+//     const result = fetchingFunction();
+//     const expected = {};
+//     t.deepEqual(result, expected);
+//     t.end();
+//   });
+
+
+test('getUserName() should return a user name', function(t) {
+    const result = gitHub();
+    const expected = "name";
+    t.deepEqual(result, expected);
+    t.end();
+  });
