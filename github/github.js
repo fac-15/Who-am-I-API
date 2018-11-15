b185617fcebb6c14419640aa5e659939f3c0b686
wright1 <prosperwright@yahoo.co.uk>
2018-11-15 10:05:54 +0000

    testing

--- github/github.js
+++ github/github.js
@@ -1,1 +1,22 @@
+//var url = "https://api.github.com/users/";// + imput value when user submits
 
+
+// (function(){
+
+//     var xhr = XMLHttpRequest();
+    
+// xhr.onreadystate = function(){
+//     if(xhr.readyState == 4 && xhr.status == 200){
+
+//         var gitResponse = xhr.responseText;
+//         var gitObject = JSON.parse(gitResponse);
+//     }
+// };
+
+// xhr.open("GET", url, true);
+// xhr.send();
+
+// })();
+
+
+module.exports = github;
--- github/github.test.js
+++ github/github.test.js
@@ -1,1 +1,10 @@
+const test = require('tape');
+const github = require( './github.js');
 
+
+test('should return an object', function(){
+    const result = fetchingFunction();
+    const expected = typeof null === 'object';
+    t.deepEqual(result, expected);
+    t.end();
+})
