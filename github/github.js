9b362b66b341a549ab0fdc177473985c6d9fd67b
wright1 <prosperwright@yahoo.co.uk>
2018-11-15 13:05:48 +0000

    tests for input function

--- github/github.js
+++ github/github.js
@@ -1,3 +1,23 @@
+function getUserName(name){
+
+    
+        let errorMess = 'Please enter name';
+        var endsWith = name.length == 0 ? true : name ;
+
+        return endsWith== true ? errorMess : 'https://api.github.com/users/'+ name ;
+
+    
+    
+}
+
+
+
+
+
+
+
+
+
 //var url = "https://api.github.com/users/";// + imput value when user submits
 
 
@@ -19,4 +39,4 @@
 // })();
 
 
-module.exports = github;
+module.exports = getUserName;
