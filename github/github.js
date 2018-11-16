3f4a96b487e3deb0d8c594671264d068acf7baa6
Sandra <grahlsan@gmail.com>
2018-11-16 09:43:46 +0000

    github avatar function

--- github/github.js
+++ github/github.js
@@ -2,88 +2,31 @@
 
 
 
+document.getElementById("form").addEventListener("submit", 
+  
+ function theRequest(event) {
+   event.preventDefault();
+  let name = document.getElementById("github-user-name").value;
 
 
-function getUserName(name){
-
-    
-  let errorMess = 'Please enter name';
-  var endsWith = name.length == 0 ? true : name ;
-
-  return endsWith== true ? errorMess : 'https://api.github.com/users/'+ name ;
-
-
-
-}
-
-const gitHubAvatar = { 
-
-theRequest: function(url, callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
+  let xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
         let obj = JSON.parse(xhr.responseText);
-        callback(obj);
+        document.getElementById("github-user-avatar").src = obj.avatar_url;
       }
     };
-    xhr.open('GET', url, true);
+    xhr.open('GET', 'https://api.github.com/users/' + name, true);
     xhr.send();
-  },
-
-userNameCallback: function(obj) {
-    document.getElementById("github-user-avatar").src = obj[0].avatar_url;
-},
-
-makeRequest: ((url, userNameCallback)=> {})()
-
-};
-
-// let fetchingFunction = (function() {
-
-//     let xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState == 4 && xhr.status == 200) {
-//             let gitHubphoto = JSON.parse(xhr.responseText);
-//             let photo = document.getElementById("#");
-//             let link = githubPhoto.avatar_url;
-//             photo.src = link;
-//         }
-//     };
-//     xhr.open("GET", url, true);
-//     xhr.send();
-//     })();
-
-
-if (typeof module !== "undefined") {
-module.exports = gitHubAvatar;
-}
+  });
 
 
 
 
 
 
-
-
-
-//var url = "https://api.github.com/users/";// + imput value when user submits
-
-
-// (function(){
-
-//     var xhr = XMLHttpRequest();
-    
-// xhr.onreadystate = function(){
-//     if(xhr.readyState == 4 && xhr.status == 200){
-
-//         var gitResponse = xhr.responseText;
-//         var gitObject = JSON.parse(gitResponse);
-//     }
-// };
-
-// xhr.open("GET", url, true);
-// xhr.send();
-
-// })();
-
+// if (typeof module !== "undefined") {
+//   module.exports = "./github/github.js";
+//   }
+  
